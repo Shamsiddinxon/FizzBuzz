13534c025fed642dfb0bf3ef528954a779a1794f
Shamsiddinxon <nosirovshamsiddinxon@gmail.com>
2021-12-30 18:54:16 +0500

    new js function

--- js/main.js
+++ js/main.js
@@ -2,23 +2,22 @@ let elForm = document.querySelector(".form");
 let elFormInput = document.querySelector(".form__input");
 let elResult = document.querySelector(".form__result");
 
+elForm.addEventListener("submit", function (evt) {
+  evt.preventDefault();
 
-elForm.addEventListener('submit', function(evt) {
-    evt.preventDefault();
+  let inputValue = elFormInput.value;
+  inputValue = inputValue * 1;
 
-    let inputValue = elFormInput.value;
-    inputValue = inputValue * 1
-
-    if (inputValue === 0) {
-        elResult.textContent = `${inputValue}`;
-    }  else if (inputValue % 3 === 0 && inputValue % 5 === 0)  {
-        elResult.textContent = "FizzBuzz";
-    } else if (inputValue % 3 === 0) {
-        elResult.textContent = "Fizz";
-    } else if (inputValue % 5 === 0) {
-        elResult.textContent = "Buzz";
-    } else {
-        elResult.textContent = `${inputValue}`;
-    }
-
-})
+  if (inputValue <= 0 || isNaN(inputValue)) {
+    elResult.textContent = null;
+    elResult.textContent = `${inputValue}`;
+  } else if (inputValue % 3 === 0 && inputValue % 5 === 0) {
+    elResult.textContent = "FizzBuzz";
+  } else if (inputValue % 3 === 0) {
+    elResult.textContent = "Fizz";
+  } else if (inputValue % 5 === 0) {
+    elResult.textContent = "Buzz";
+  } else {
+    elResult.textContent = `${inputValue}`;
+  }
+});
